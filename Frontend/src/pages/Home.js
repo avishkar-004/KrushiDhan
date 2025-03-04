22a6c3da8a0cf0b2d7c529fde2738acb6e351b7f
Avishkar Pawar <avishkarpawar004@gmail.com>
2025-03-04 14:30:00 +0530

    feat: add Home page component

---
+++ Frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Krishi-Bandhu</h1>
+      <p>Helping farmers with technology</p>
+    </div>
+  );
+};
+
+export default Home;
